7b7eea592a5c3b840176278a125322dd358793f8
Boutissante Issam <boutissante.issam.dev@gmail.com>
2020-10-31 17:10:00 +0100

    adding the main program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConnectToDataBase
+{
+    class Program
+    {
+        static readonly string mon_dataBase = ConfigurationManager.ConnectionStrings["monDataBase"].ConnectionString;
+        static void Main(string[] args)
+        {
+            SqlConnection mon_connection = new SqlConnection(mon_dataBase);
+            using (mon_connection)
+            {
+                mon_connection.Open();
+                Console.WriteLine("la connection est : " + mon_connection.State);
+            }
+            Console.WriteLine("la connection est : " + mon_connection.State);
+            Console.ReadKey();
+        }
+    }
+}
